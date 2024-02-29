19712e6889bdc07b5928ebec5d06b679bf2b89b9
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-02-29 16:26:40 -0400

    working but inefficient

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function showDiv(divname) {
+  document.getElementById("rebelalliance").style.display = "none";
+  document.getElementById("kruleboyz").style.display = "none";
+  document.getElementById("kharadron").style.display = "none";
+  document.getElementById("tauempire").style.display = "none";
   document.getElementById(divname).style.display = "block";
 }
