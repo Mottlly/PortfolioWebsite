6bd9de1b36d0180f73b142c52397de03dc9ac053
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-03-02 13:06:15 -0400

    faction tab disappears on re-click

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function showDiv(divname) {
-  document.getElementById("rebelalliance").style.display = "none";
-  document.getElementById("kruleboyz").style.display = "none";
-  document.getElementById("kharadron").style.display = "none";
-  document.getElementById("tauempire").style.display = "none";
-  document.getElementById(divname).style.display = "block";
+  if (document.getElementById(divname).style.display === "block") {
+    document.getElementById(divname).style.display = "none";
+  } else {
+    document.getElementById("rebelalliance").style.display = "none";
+    document.getElementById("kruleboyz").style.display = "none";
+    document.getElementById("kharadron").style.display = "none";
+    document.getElementById("tauempire").style.display = "none";
+    document.getElementById(divname).style.display = "block";
+  }
 }
